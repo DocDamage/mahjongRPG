90558ca3f149233c15a7716242840350a75a3f4e
DocDamage <dsferraro@gmail.com>
2026-08-02 13:01:29 -0400

    Add connected Riverbend fishing location

--- src/world/wayward_farm.gd
+++ src/world/wayward_farm.gd
@@ -18,6 +18,7 @@ func _ready() -> void:
 	$ShippingCrate.feedback.connect(_show_message)
 	$DustwardRoad.feedback.connect(_show_message)
 	$WaywardHitch.feedback.connect(_show_message)
+	$RiverbendPath.feedback.connect(_show_message)
 	_update_status(GameSession.day, GameSession.minute_of_day)
 
 
